72a480de9cb459a89e7eb4f0d55058dc55e0e68c
BeestingGit <anthonypaulchong@gmail.com>
2025-02-28 14:17:40 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CalculatorApp',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: CalculatorHomePage(),
+    );
+  }
+}
+
+class CalculatorHomePage extends StatefulWidget {
+  @override
+  _CalculatorHomePageState createState() => _CalculatorHomePageState();
+}
+
+class _CalculatorHomePageState extends State<CalculatorHomePage> {
+  String display = '';
+  double operand1 = 0;
+  double operand2 = 0;
+  String operator = '';
+}
